d8ea31daa0b935293ec2f927b2d79fdaa74210e0
Romulo Assis <romulo.assis@ifood.com.br>
2023-03-24 18:51:35 -0300

    step 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+type GitHubApi = {
+  name: string;
+  description: string;
+  html_url: string;
+  visibility: boolean;
+};
+
+export const App = () => {
+  const [repos, setRepos] = useState<GitHubApi[]>([]);
+
+  useEffect(() => {
+    const getApiData = async () => {
+      const response = await fetch(
+        "https://api.github.com/users/RAssis7/repos"
+      ).then((response) => response.json());
+
+      setRepos(response);
+    };
+    getApiData();
+  }, []);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+    <main>
+      {repos.map(({ name, description, html_url, visibility }) => (
+        <fieldset>
+          <a href={html_url} target="_blank" rel="">
+            {name}
+          </a>
+          <p>{description}</p>
+          <strong>{visibility ? "Visible" : "Invisible"}</strong>
+        </fieldset>
+      ))}
+    </main>
+  );
+};
 
-export default App
+export default App;
